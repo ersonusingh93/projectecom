7f04e5fec84462e3f0f3b04b3b83e5cc1d6ed393
Sonu Singh <iamersonusingh@gmail.com>
2025-08-05 13:20:20 +0530

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 name = "Artificial Intelligence"
 
+import numpy as np 
+import pandas as pd 
+
+
 
 print(f"Welcome to the world of {name}")
+
+arr1 = np.random.randint(25, size=(5,5))
+
+
+print(arr1)
